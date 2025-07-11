161b127382c626da82c23977167471038906ef8b
lequang <minh.quangle15111995@gmail.com>
2025-07-11 13:05:30 +0900

    Update SinglyLinkedList.c
    add method add_item_at to link list

--- LinkedList/SinglyLinkedList.c
+++ LinkedList/SinglyLinkedList.c
@@ -67,12 +67,45 @@ void traverse_list(struct LinkedList *list)
     }
 };
 
+void add_item_at(struct LinkedList *list, void *data, int position)
+{
+    if (position < 0 || position > list->size)
+    {
+        printf("Position %d is invalid!\n", position);
+        return;
+    }
+    
+    struct Node *temp = list->head;
+    struct Node *new_node = node_constructor(data);
+    
+    if (position == 0)
+    {
+        new_node->next = list->head;
+        list->head = new_node;
+        list->size++;
+        return;
+    }
+    
+    int i;
+    for (i = 0; i <= position; i++)
+    {
+        temp = temp->next;
+    }
+    new_node->next = temp->next;
+        temp->next = new_node;
+        list->size++;
+        return;
+}
+
 int main() {
     int value = 1;
     int value2 = 2;
     struct LinkedList *list = linked_list_constructor();
     add_item(list, &value);
     add_item(list, &value2);
+    // traverse_list(list);
+    int value3 = 3;
+    add_item_at(list, &value3, 0);
     traverse_list(list);
     free(list);
     return 0;
